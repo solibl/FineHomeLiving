fca76a385d467101eb9b077e39d03b9665776f17
Brian Lee <brianleedongjun@gmail.com>
2017-06-13 16:03:04 -0700

    Refactor migrations for users, albums, and images

--- db/migrate/20170613223235_create_albums.rb
+++ db/migrate/20170613223235_create_albums.rb
@@ -1,11 +1,12 @@
 class CreateAlbums < ActiveRecord::Migration[5.1]
   def change
     create_table :albums do |t|
-      t.string :title
-      t.string :location
-      t.text :album_description
+      t.string :title, null: false
+      t.string :location, null: false
+      t.text :album_description, null: false
+      t.attachment :image
 
-      t.timestamps
+      t.timestamps, null: false
     end
   end
 end
--- db/migrate/20170613223900_create_images.rb
+++ db/migrate/20170613223900_create_images.rb
@@ -1,11 +1,12 @@
 class CreateImages < ActiveRecord::Migration[5.1]
   def change
     create_table :images do |t|
-      t.string :name
-      t.text :description
+      t.string :name, null: false
+      t.text :description, null: false
+      t.attachment :image
       t.references :album, foreign_key: true
       
-      t.timestamps
+      t.timestamps, null: false
     end
   end
 end
