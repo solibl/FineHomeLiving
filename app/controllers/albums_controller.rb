ff589370a8e93fe1d6f847420b46c75b77b89f5b
Brian Lee <brianleedongjun@gmail.com>
2017-06-13 19:37:13 -0700

    Add validation to image upload for paperclip. Add form to create a new album listing for a project

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,29 @@
 class AlbumsController < ApplicationController
 	def index
+		@albums = Album.all
+	end
+
+	def create
+
+	end
+
+	def new
+		@album = Album.new
+	end
+
+	def edit
+
+	end
+
+	def show
+
+	end
+
+	def update
+
+	end
+
+	def destroy
 
 	end
 end
