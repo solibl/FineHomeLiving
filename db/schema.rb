5b741d1aa9e2bbaffcdd99b440e0cf294794b649
Brian Lee <brianleedongjun@gmail.com>
2017-06-13 19:54:20 -0700

    Add image table column for paperclip in Image table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614024954) do
+ActiveRecord::Schema.define(version: 20170614025320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20170614024954) do
     t.bigint "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["album_id"], name: "index_images_on_album_id"
   end
 
