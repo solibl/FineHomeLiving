24e932e06bdfbae4aff852cb51ba61474c8fdf98
Brian Lee <brianleedongjun@gmail.com>
2017-06-14 13:42:35 -0700

    Add image route to make add new image for an album

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,8 @@
 class Image < ApplicationRecord
-  belongs_to :album
+	belongs_to :album
+
+	validates :title, :location, :album_description, {presence: true}
+	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>"}, default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
