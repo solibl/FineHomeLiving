ba5ec5ff19bca265fa0234ee9aa9a796fca28c3e
Brian Lee <brianleedongjun@gmail.com>
2017-09-10 02:12:51 -0700

    Refactor code to test mailing errors

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,4 +1,6 @@
 class MainsController < ApplicationController
+	include SendGrid
+	
 	def index
 		@albums = Album.all
 		@images = Image.all
@@ -7,13 +9,16 @@ class MainsController < ApplicationController
 	end
 
 	def create
-		@contact = Contact.new(params[:contact])
-		@contact.request = request
-		if @contact.deliver
-			flash.now[:success] = "The email has been sent"
-		else
-			flash.now[:error] = "Can not send message."
-			redirect_to root_path
-		end
+		from = Email.new(email: params[:contact]["email"])
+		subject = 'From FineHomeLiving.com'
+		to = Email.new(email: 'brianleedongjun@gmail.com')
+		content = Content.new(type: 'text/plain', value: params[:contact]["message"])
+		mail = Mail.new(from, subject, to, content)
+
+		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+		response = sg.client.mail._('send').post(request_body: mail.to_json)
+		puts response.status_code
+		puts response.body
+		puts response.headers
 	end
 end
