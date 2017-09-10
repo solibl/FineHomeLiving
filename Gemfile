dfd6cd1f8062982b9f97b498c6b6b8ca51e67daa
Brian Lee <brianleedongjun@gmail.com>
2017-09-10 01:49:11 -0700

    add apikey environment for mailer

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'mail_form', '~> 1.5', '>= 1.5.1'
-
+gem 'sendgrid-ruby'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,23 @@
 class Contact < MailForm::Base
+	require 'sendgrid-ruby'
+	include SendGrid
 	attribute :name, 		validate: true
 	attribute :email, 		validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 	attribute :message, 	validate: true
 	attribute :nickname, 	validate: true	
 
+	from = Email.new(email: 'test@example.com')
+	subject = 'Hello World from the SendGrid Ruby Library!'
+	to = Email.new(email: 'test@example.com')
+	content = Content.new(type: 'text/plain', value: 'Hello, Email!')
+	mail = Mail.new(from, subject, to, content)
+
+	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	puts response.status_code
+	puts response.body
+	puts response.headers
+	
 	def headers
 		{
 			subject: "Contact Form",
@@ -11,4 +25,5 @@ class Contact < MailForm::Base
 			from: %("#{name}" <#{email})
 		}
 	end
+
 end
