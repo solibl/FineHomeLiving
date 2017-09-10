3d9d0d4c7ef6ecea633534a345d0f985df75a7fa
Brian Lee <brianleedongjun@gmail.com>
2017-09-10 01:52:58 -0700

    refactor controller for mailer

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -8,7 +8,17 @@ class MainsController < ApplicationController
 
 	def create
 		@contact = Contact.new
-		@contact.request = request
+		from = Email.new(email: 'test@example.com')
+		subject = 'Hello World from the SendGrid Ruby Library!'
+		to = Email.new(email: 'test@example.com')
+		content = Content.new(type: 'text/plain', value: 'Hello, Email!')
+		mail = Mail.new(from, subject, to, content)
+
+		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+		response = sg.client.mail._('send').post(request_body: mail.to_json)
+		puts response.status_code
+		puts response.body
+		puts response.headers
 		if @contact.deliver
 			flash.now[:success] = "The email has been sent"
 		else
