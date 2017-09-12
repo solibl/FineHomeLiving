346140f27db2e73e8661c3fb07be8cefd11459bd
Brian Lee <brianleedongjun@gmail.com>
2017-09-11 23:03:40 -0700

    Refactor code to show picture from aws s3

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,6 @@
 	has_many :images
 
 	validates :title, :location, :album_description, :price, {presence: true}
-	has_attached_file :image, styles: { large: "1920x1080>", medium: "300x300>"}, default_url: "/app/assets/images/:style/missing.png"
+	has_attached_file :image, styles: { large: "1920x1080>", medium: "300x300>"}, :url =>':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename'
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
