e8dd87b656113389e5a1f457b36a83a91ff0da3a
Brian Lee <brianleedongjun@gmail.com>
2017-09-11 23:07:30 -0700

    Refactor code to show images on the heroku app from s3 on the albums section

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ApplicationRecord
 	belongs_to :album
 
 	validates :name, :description, {presence: true}
-	has_attached_file :image, styles: { large: "1000x1000>", medium: "300x300>"}, default_url: "/app/assets/images/:style/missing.png"
+	has_attached_file :image, styles: { large: "1000x1000>", medium: "300x300>"}, :url =>':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename'
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
  	do_not_validate_attachment_file_type :image
 end
